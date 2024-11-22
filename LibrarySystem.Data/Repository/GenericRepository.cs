8c99dde6891c2f3dfa1ce1e72ce7f849465201fd
fathyali11 <fathy.ali8ali@gmail.com>
2024-11-22 13:16:29 +0200

    enhance generic repos

--- LibrarySystem.Data/Repository/GenericRepository.cs
+++ LibrarySystem.Data/Repository/GenericRepository.cs
@@ -1,9 +1,10 @@
 ﻿using LibrarySystem.Data.Data;
 using LibrarySystem.Domain.IRepository;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace LibrarySystem.Data.Repository;
-internal class GenericRepository<T> : IGenericRepository<T> where T : class
+public class GenericRepository<T> : IGenericRepository<T> where T : class
 {
     private readonly ApplicationDbContext _context;
     private readonly DbSet<T> _dbSet;
@@ -12,17 +13,67 @@ internal class GenericRepository<T> : IGenericRepository<T> where T : class
         _context = context;
         _dbSet = _context.Set<T>();
     }
-    public async Task<IEnumerable<T>> GetAllAsync()
+    public async Task<IEnumerable<T>> GetAllAsync(
+        Expression<Func<T, bool>>? predicate = null,
+        string? includedNavigations = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+        string? searchTerm = null,
+        Expression<Func<T, bool>>? searchExpression = null,
+        CancellationToken cancellationToken = default)
     {
-        return await _dbSet.ToListAsync();
+        IQueryable<T> query = _dbSet.AsQueryable();
+        if (predicate is not null)
+        {
+            query = query.Where(predicate);
+        }
+
+        
+        if (includedNavigations is not null)
+        {
+            foreach (var includedNavigation in includedNavigations.Split(","))
+            {
+                query = query.Include(includedNavigation);
+            }
+        }
+
+        
+        if (!string.IsNullOrEmpty(searchTerm) && searchExpression is not null)
+        {
+            query = query.Where(searchExpression);
+        }
+
+        
+        if (orderBy is not null)
+        {
+            query = orderBy(query);
+        }
+
+       
+        return await query.ToListAsync(cancellationToken);
+    }
+
+    public async Task<T?> GetByAsync(Expression<Func<T, bool>>? predicate, string? includedNavigations, CancellationToken cancellationToken = default)
+    {
+        IQueryable<T> query = _dbSet.AsQueryable();
+        if (predicate is not null)
+        {
+            query = query.Where(predicate);
+        }
+        if (includedNavigations is not null)
+        {
+            foreach (var includedNavigation in includedNavigations.Split(","))
+                query = query.Include(includedNavigation);
+        }
+        return await query.FirstOrDefaultAsync(cancellationToken);
     }
     public async Task<T?> GetByIdAsync(int id)
     {
         return await _dbSet.FindAsync(id);
     }
-    public async Task AddAsync(T entity)
+    
+    public async Task AddAsync(T entity, CancellationToken cancellationToken = default)
     {
-        await _dbSet.AddAsync(entity);
+        await _dbSet.AddAsync(entity,cancellationToken);
     }
 
     public void Delete(T entity)
@@ -30,7 +81,10 @@ internal class GenericRepository<T> : IGenericRepository<T> where T : class
         _dbSet.Remove(entity);
     }
 
-    
+    public void DeleteRange(IEnumerable<T> entities)
+    {
+        _dbSet.RemoveRange(entities);
+    }
 
-    
+
 }
--- LibrarySystem.Domain/IRepository/IGenericRepository.cs
+++ LibrarySystem.Domain/IRepository/IGenericRepository.cs
@@ -1,8 +1,18 @@
-﻿namespace LibrarySystem.Domain.IRepository;
+﻿using System.Linq.Expressions;
+
+namespace LibrarySystem.Domain.IRepository;
 public interface IGenericRepository<T> where T : class
 {
-    Task<IEnumerable<T>> GetAllAsync();
+    Task<IEnumerable<T>> GetAllAsync(
+         Expression<Func<T, bool>>? predicate = null,
+         string? includedNavigations = null,
+         Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+         string? searchTerm = null,
+         Expression<Func<T, bool>>? searchExpression = null,
+         CancellationToken cancellationToken = default);
+    Task<T?> GetByAsync(Expression<Func<T, bool>>? predicate, string? includedNavigations, CancellationToken cancellationToken = default);
     Task<T?> GetByIdAsync(int id);
-    Task AddAsync(T entity);
+    Task AddAsync(T entity, CancellationToken cancellationToken = default);
     void Delete(T entity);
+    void DeleteRange(IEnumerable<T> entities);
 }
