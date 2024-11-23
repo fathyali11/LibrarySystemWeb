8c97a336b6041ed8cc518a3a821ee9029ae7a9fc
fathyali11 <fathy.ali8ali@gmail.com>
2024-11-23 17:21:46 +0200

    add validation for category request

---
+++ LibrarySystem.Domain/FluentValidations/Categories/CategoryRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using LibrarySystem.Domain.DTO.Categories;
+namespace LibrarySystem.Domain.FluentValidations.Categories;
+public class CategoryRequestValidator: AbstractValidator<CategoryRequest>
+{
+    public CategoryRequestValidator()
+    {
+        RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MaximumLength(100).WithMessage("Name cannot exceed 100 characters.");
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required.")
+            .MaximumLength(500).WithMessage("Description cannot exceed 500 characters.");
+    }
+}
