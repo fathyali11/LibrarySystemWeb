247262b5721f58d8bd7cc42380185159d1ff4c60
fathy <fathy@FATHYALI>
2024-11-25 02:40:24 +0200

    comment

--- Library.Web/Program.cs
+++ Library.Web/Program.cs
@@ -16,7 +16,7 @@ using Serilog;
 var builder = WebApplication.CreateBuilder(args);
 
 
-
+//
 Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)  
     .CreateLogger();
