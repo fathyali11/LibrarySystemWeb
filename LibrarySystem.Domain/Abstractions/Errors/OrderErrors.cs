b1b50fb0fdc821c779fcfbbe5ae73e2483b192cd
fathy <fathyali11>
2024-12-06 22:00:44 +0200

    add error

--- LibrarySystem.Domain/Abstractions/Errors/OrderErrors.cs
+++ LibrarySystem.Domain/Abstractions/Errors/OrderErrors.cs
@@ -5,6 +5,7 @@ namespace LibrarySystem.Domain.Abstractions.Errors
     public static class OrderErrors
     {
         public static readonly Error NotFound = new("Order.NotFound", "This Order is not found", StatusCodes.Status400BadRequest);
+        public static readonly Error NotEnoughQuantity = new("Order.LargeQuantity", "This Order quantity greater than quantity of this book! please decrease it", StatusCodes.Status400BadRequest);
 
 
 
