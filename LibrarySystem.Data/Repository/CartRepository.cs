a521196872692fb9b6db963755189a310f306819
fathy <fathyali11>
2024-12-16 00:32:13 +0200

    add repo for cart

---
+++ LibrarySystem.Data/Repository/CartRepository.cs
@@ -0,0 +1,8 @@
+﻿using LibrarySystem.Data.Data;
+using LibrarySystem.Domain.Entities;
+using LibrarySystem.Domain.IRepository;
+
+namespace LibrarySystem.Data.Repository;
+public class CartRepository(ApplicationDbContext context) : GenericRepository<Cart>(context), ICartRepository
+{
+}
