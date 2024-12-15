1259db2c3770279b3cb0c383ae958237d3948bd3
fathy <fathyali11>
2024-12-16 01:38:56 +0200

    create mapping

---
+++ LibrarySystem.Domain/Mappings/CartItemMappings.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using LibrarySystem.Domain.DTO.CartItems;
+using LibrarySystem.Domain.Entities;
+
+namespace LibrarySystem.Domain.Mappings;
+public class CartItemMappings:Profile
+{
+    public CartItemMappings()
+    {
+        CreateMap<CartItemRequest, CartItem>()
+            .ForMember(dest => dest.BookId, option => option.MapFrom(src => src.BookId))
+            .ForMember(dest => dest.OrderType, option => option.MapFrom(src => src.Type))
+            .ForMember(dest => dest.Quantity, option => option.MapFrom(src => src.Quantity))
+            .ReverseMap();
+    }
+}
