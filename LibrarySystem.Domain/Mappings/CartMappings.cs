18aa13039ca3620fa395a85b20e8f074a66a8cb3
fathy <fathyali11>
2024-12-17 22:21:05 +0200

    map from cart to response

---
+++ LibrarySystem.Domain/Mappings/CartMappings.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using LibrarySystem.Domain.DTO.Carts;
+using LibrarySystem.Domain.Entities;
+
+namespace LibrarySystem.Domain.Mappings;
+public class CartMappings:Profile
+{
+    public CartMappings()
+    {
+        CreateMap<Cart, CartReponse>()
+            .ForMember(dest => dest.Id, option => option.MapFrom(src => src.Id))
+            .ForMember(dest => dest.CartItems, option => option.MapFrom(src => src.CartItems))
+            .ReverseMap();
+    }
+}
