b7d133b48fda63bcd61759de3da905d0f308ccdc
fathy <fathyali11>
2024-12-17 22:12:45 +0200

    add error

---
+++ LibrarySystem.Domain/Abstractions/Errors/CartErrors.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace LibrarySystem.Domain.Abstractions.Errors
+{
+    public static class CartErrors
+    {
+        public static readonly Error NotFound = new("Cart.NotFound", "This Cart is not found", StatusCodes.Status400BadRequest);
+
+
+
+
+
+
+
+
+
+
+    }
+}
