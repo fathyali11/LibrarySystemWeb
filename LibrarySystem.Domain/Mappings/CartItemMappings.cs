a2ff2934a760aa88c0dc1fed3030ee319a018de4
fathy <fathyali11>
2024-12-17 22:21:20 +0200

    map from cart item to response

--- LibrarySystem.Domain/Mappings/CartItemMappings.cs
+++ LibrarySystem.Domain/Mappings/CartItemMappings.cs
@@ -12,5 +12,15 @@ public class CartItemMappings:Profile
             .ForMember(dest => dest.OrderType, option => option.MapFrom(src => src.Type))
             .ForMember(dest => dest.Quantity, option => option.MapFrom(src => src.Quantity))
             .ReverseMap();
+
+        CreateMap<CartItem, CartItemResponse>()
+            .ForMember(dest => dest.Id, option => option.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Type, option => option.MapFrom(src => src.OrderType))
+            .ForMember(dest => dest.Quantity, option => option.MapFrom(src => src.Quantity))
+            .ForMember(dest => dest.Name, option => option.MapFrom(src => src.Book.Title))
+            .ForMember(dest => dest.Price, option => option.MapFrom(src => src.Price))
+            .ForMember(dest => dest.TotalPrice, option => option.MapFrom(src => src.Quantity*src.Price))
+            .ForMember(dest => dest.ImageUrl, option => option.MapFrom(src => src.Book.ImagePath))
+            .ReverseMap();
     }
 }
