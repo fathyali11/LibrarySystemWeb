f57766efb2b6705d10d5f8dc50dc7654889a7fd9
fathy <fathyali11>
2024-12-17 04:03:11 +0200

    add record and its validation

---
+++ LibrarySystem.Domain/FluentValidations/CartItems/PlusCartItemRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using LibrarySystem.Domain.DTO.CartItems;
+
+namespace LibrarySystem.Domain.FluentValidations.CartItems;
+public class PlusCartItemRequestValidator:AbstractValidator<PlusCartItemRequest>
+{
+    public PlusCartItemRequestValidator()
+    {
+        RuleFor(x => x.CartId)
+            .NotEmpty().WithMessage("CartId is required")
+            .GreaterThan(0).WithMessage("CartId must be greater than zero");
+
+        RuleFor(x => x.OrderId)
+            .NotEmpty().WithMessage("OrderId is required")
+            .GreaterThan(0).WithMessage("OrderId must be greater than zero");
+
+    }
+}
