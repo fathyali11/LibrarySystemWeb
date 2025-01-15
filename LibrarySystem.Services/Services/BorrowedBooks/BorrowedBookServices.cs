7119f9d56d3ecd1e2c30a990298ad47806ae3937
fathy <fathyali11>
2025-01-15 09:31:13 +0200

    xml for borrowed books services

--- LibrarySystem.Services/Services/BorrowedBooks/BorrowedBookServices.cs
+++ LibrarySystem.Services/Services/BorrowedBooks/BorrowedBookServices.cs
@@ -1,11 +1,13 @@
 ﻿using LibrarySystem.Domain.DTO.BorrowBooks;
 
 namespace LibrarySystem.Services.Services.BorrowedBooks;
+/// <include file='ExternalServicesDocs\BorrowedBooksDocs.xml' path='/docs/members[@name="borrowedBookServices"]/BorrowedBookServices'/>
 public class BorrowedBookServices(ApplicationDbContext context,
     IUnitOfWork unitOfWork,HybridCache hybridCache): BorrowedBookRepository(context), IBorrowedBookServices
 {
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
     private readonly HybridCache _hybridCache = hybridCache;
+    /// <include file='ExternalServicesDocs\BorrowedBooksDocs.xml' path='/docs/members[@name="borrowedBookServices"]/ReturnBookAsync'/>
     public async Task<OneOf<bool,Error>> ReturnBookAsync(string userId,int borrowedBookId,CancellationToken cancellationToken=default)
     {
         if(borrowedBookId <= 0)
@@ -23,6 +25,7 @@ public class BorrowedBookServices(ApplicationDbContext context,
         await _hybridCache.RemoveAsync(GeneralConsts.AllAvailableBooksCachedKey, cancellationToken);
         return true;
     }
+    /// <include file='ExternalServicesDocs\BorrowedBooksDocs.xml' path='/docs/members[@name="borrowedBookServices"]/GetAllBorrowedBooksAsync'/>
     public async Task<List<borrowedBookResponse>> GetAllBorrowedBooksAsync(CancellationToken cancellationToken=default)
     {
         var result = await _unitOfWork.BorrowedBookRepository.GetAllWithUserAndBookForDisplay(cancellationToken);
