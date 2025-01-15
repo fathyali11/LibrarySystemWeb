c0e015a54e791663e41a8104414a64b7c60df976
fathy <fathyali11>
2025-01-15 13:28:59 +0200

    remove distributed cashe

--- LibrarySystem.Services/Services/Cashing/ICacheServices.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LibrarySystem.Services.Services.Cashing
-{
-    public interface ICacheServices
-    {
-        Task SetAsync<T>(string key, T value,CancellationToken cancellationToken=default);
-        Task<T?> GetAsync<T>(string key,CancellationToken cancellationToken=default);
-        Task RemoveAsync(string key, CancellationToken cancellationToken = default);
-    }
-}
